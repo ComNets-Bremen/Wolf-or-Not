11487cbcfc1ba4343db19af7a533f9dc01ee9ff8
Jens Dede <jd@comnets.uni-bremen.de>
2022-08-24 09:24:18 +0200

    Updated settings

--- labelserver/settings.py
+++ labelserver/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), # your static/ files folder
-]
+
+STATIC_ROOT=os.path.join(BASE_DIR, "static") # your static/ files folder
+
+MEDIA_ROOT=BASE_DIR
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
