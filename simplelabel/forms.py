84ac38d253d84d7fc12276331de399addeacd549
Jens Dede <jd@comnets.uni-bremen.de>
2022-11-21 15:43:33 +0100

    Use js media for upload protection. New download form

--- simplelabel/forms.py
+++ simplelabel/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.templatetags.static import static
+from django.db.models import Count
 
-from .models import Image, Property
+
+from .models import Image, Property, Dataset
 
 #class FileFieldForm(forms.Form):
 #    project_name = forms.CharField()
@@ -12,13 +15,14 @@ class FileFieldForm(forms.ModelForm):
         model = Image
         fields = ("image_dataset","image")
 
+    class Media:
+        js = (static('js/upload_form.js'),)
+
     def __init__(self, *args, **kwargs):
         super(FileFieldForm, self).__init__(*args, **kwargs)
         self.fields["image"].widget.attrs["multiple"] = True
 
-
 class PollForm(forms.Form):
-
     def __init__(self, *args, **kwargs):
         super(PollForm, self).__init__(*args, **kwargs)
         props = Property.objects.all().values_list("id", "property_name")
@@ -28,3 +32,27 @@ class PollForm(forms.Form):
             widget=forms.CheckboxSelectMultiple,
             choices=props,
             )
+
+class DownloadForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        dataset_ids = Image.objects.all().values("image_dataset").distinct()
+        choices = Dataset.objects.filter(id__in=dataset_ids).values_list("id", "dataset_name")
+
+        p = Image.objects.all().annotate(num_polls=Count("poll", distinct=True)).order_by("-num_polls").values_list("num_polls").distinct()
+        num_polls = [(i, "At least "+str(i) + " polls") for i in range(max(p)[0]+1)]
+
+
+        self.fields["datasets"] = forms.MultipleChoiceField(
+                label="Use these datasets",
+                required=True,
+                widget=forms.CheckboxSelectMultiple,
+                choices=choices,
+                )
+
+        self.fields["num_polls"] = forms.CharField(
+                label="Min number of polls",
+                required = True,
+                widget=forms.Select(choices=num_polls),
+                )
