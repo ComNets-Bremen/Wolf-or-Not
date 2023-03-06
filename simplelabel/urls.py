b86d16018c9490dd64dcfd96314da42326a49f3b
Jens Dede <jd@comnets.uni-bremen.de>
2023-03-06 23:09:42 +0100

    Do not create square images for download

--- simplelabel/urls.py
+++ simplelabel/urls.py
@@ -5,11 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('img/<uuid:uuid>/', views.get_image, name='get_image'),
-    path('original-img/<uuid:uuid>/', views.get_image, {"max_size":None}, name='get_original_image'),
+    path('original-img/<uuid:uuid>/', views.get_image, {"max_size":None, "force_squared":False}, name='get_original_image'),
     path('poll', views.PollImageView.as_view(), name='poll'),
     path('upload', views.UploadImagesView.as_view(), name='upload_images'),
     path('statistics', views.get_statistics, name="get_statistics"),
     path('download', views.DownloadView.as_view(), name='download_results'),
 ]
-
 
