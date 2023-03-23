6f8c9b65cbc0655ff460b6a2313d0de13c693455
Jens Dede <jd@comnets.uni-bremen.de>
2023-03-23 23:37:29 +0100

    Changed upload: File by file, mass upload should work now. Fixes #2

--- static/js/upload_form.js
+++ static/js/upload_form.js
@@ -1,13 +1,55 @@
 /*
- * Limit number of uploaded files to a certain number
+ * Upload image by image
  */
 
+num_elements = 0;
+const forms = [];
+
+function sendForms() {
+    const statusDiv = document.getElementById("uploadStatus");
+    let form = forms.pop();
+    if (form) {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener("load", (event) => {
+            statusDiv.innerHTML = "Upload status: " + Math.round((num_elements - forms.length)/num_elements*100) + "%";
+            setTimeout(function() {sendForms();}, 5); // Give the browser time to render the UI
+        });
+
+        request.addEventListener("error", (event) => {
+            statusDiv.innerHTML = "Upload Error";
+        });
+
+        request.open("POST", "", false);
+        request.send(form);
+    } else {
+        statusDiv.innerHTML="Upload Done";
+    }
+}
+
 window.onload = function () {
-    const file_input = document.getElementById('id_image');
-    file_input.addEventListener('change', (event) => {
-        if (file_input.files.length >= 1000){
-            alert("Maximum 1000 files can be uploaded at once. Selected number of files: " + file_input.files.length);
-            file_input.value = "";
-        }
+    const form = document.getElementById("uploadForm");
+
+    form.addEventListener("submit", (e) => {
+        const statusDiv = document.getElementById("uploadStatus");
+
+        statusDiv.style.display = '';
+        form.style.display="none";
+
+        const images = document.getElementById("id_image");
+        let max_files = images.files.length;
+
+        Array.from(images.files).forEach((file, i) => {
+            const fd = new FormData(form);
+            fd.set("image", file);
+            forms.push(fd);
+        });
+        form.reset();
+        num_elements = forms.length;
+        e.preventDefault();
+        sendForms();
     });
 }
+
+
+
